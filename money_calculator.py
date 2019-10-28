0d2a94cb286607a0216c6b92c10194c2f50ff039
GorIsoyan <48748144+GorIsoyan@users.noreply.github.com>
2019-10-29 03:10:12 +0400

    Update money_calculator.py

--- money_calculator.py
+++ money_calculator.py
@@ -1,8 +1,11 @@
 import json
 
+
 class User:
-    def __init__(self, name):
+    print("\nWelcome to MoneySaver program")
+    def __init__(self, name, password):
         self.name = name
+        self.password = password
         self.spendings = []
 
     def addSpending(self, activityType, dayOfWeek, amount):
@@ -15,31 +18,59 @@ class User:
     def printSpendings(self):
         print(self.spendings)
 
-def main():
+    @staticmethod
+    def action():
+        print("\nPlease select one of these actions: ")
+        actions = int(input("1–press for adding, 2–for the report, 3–exist "))
+        if actions == 1 or actions == 2:
+            return actions
+        elif actions == 3:
+            print("You have exited ")
+            return actions
+        else:
+            print("You have wrong input try again: ")
+            return actions
 
-    userName = input("Please let us know your Name")
-    user = User(userName)
-    
+    @classmethod
+    def set_up_json(cls):
+        with open("main.json") as data_file:
+            file = json.loads(data_file)
+            cls.activity = file['student']['activity']
+            cls.weekdays = file['student']['weekdays']
+
+
+class Activity(User):
+    def use_activity(self):
+        key = input("Please choose one of the activities: ")
+        while self.activity:
+            if key in self.activity:
+                return key
+            else:
+                print("You type something wrong, Try again ")
+                return key
+
+
+
+
+
+def main():
+    User.action()
+    userName = input("\nPlease let us know your Name: ")
+    userPassword = input("\nPlease enter your Password: ")
+    user = User(userName, userPassword)
+
     while True:
         choice = input("Do you want to add Spending? please type Yes/No")
-        if (choice == "No"):
+        if choice == "No":
             user.printSpendings()
             print("Thanks for using our App")
             break
-        else: 
-            activity = input("Please type the activity")
-            day = input("Please type the day of the week  ")
-            amount = input("Please type the amount ")
-            
+        else:
+            activity = Activity.use_activity()
+            day = input("Please type the day of the week: ")
+            amount = input("Please type the amount: ")
+
             user.addSpending(activity, day, amount)
 
+
 main()
-
-
-
-
-
-
-
-
-
