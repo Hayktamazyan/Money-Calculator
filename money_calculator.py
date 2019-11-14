4f1982f22abe02ecd0c7e006450165a6e8b3371a
GorIsoyan <48748144+GorIsoyan@users.noreply.github.com>
2019-11-14 22:11:15 +0400

    Update money_calculator.py

--- money_calculator.py
+++ money_calculator.py
@@ -3,74 +3,81 @@ import json
 
 class User:
     print("\nWelcome to MoneySaver program")
+
+    file = "main.json"
+
     def __init__(self, name, password):
         self.name = name
         self.password = password
         self.spendings = []
 
-    def addSpending(self, activityType, dayOfWeek, amount):
+    def add_spending(self, activityType, dayOfWeek, amount):
         newSpending = {}
         newSpending["activity"] = activityType
         newSpending["day"] = dayOfWeek
         newSpending["amount"] = amount
         self.spendings.append(newSpending)
 
-    def printSpendings(self):
+    def print_spendings(self):
         print(self.spendings)
 
-    @staticmethod
-    def action():
-        print("\nPlease select one of these actions: ")
-        actions = int(input("1–press for adding, 2–for the report, 3–exist "))
-        if actions == 1 or actions == 2:
-            return actions
-        elif actions == 3:
-            print("You have exited ")
-            return actions
-        else:
-            print("You have wrong input try again: ")
-            return actions
-
-    @classmethod
-    def set_up_json(cls):
-        with open("main.json") as data_file:
+    def set_up_json(self, file, activities, weekdays):
+        self.activities = activities
+        self.weekdays = weekdays
+        with open(file) as data_file:
             file = json.loads(data_file)
-            cls.activity = file['student']['activity']
-            cls.weekdays = file['student']['weekdays']
+            self.activities = file['student']['activity']
+            self.weekdays = file['student']['weekdays']
+            return self.activities, self.weekdays
+
+
+def action():
+    print("\nPlease select one of these actions: ")
+    actions = int(input("1–press for adding, 2–for the report, 3–exist "))
+    if actions == 1 or actions == 2:
+        return actions
+    elif actions == 3:
+        print("You have exited ")
+        return actions
+    else:
+        print("You have wrong input try again: ")
+        return actions
 
 
 class Activity(User):
-    def use_activity(self):
+    def choose_activity(self):
         key = input("Please choose one of the activities: ")
-        while self.activity:
-            if key in self.activity:
-                return key
-            else:
-                print("You type something wrong, Try again ")
-                return key
+        if key in self.activities:
+            return key
+        else:
+            print("You have a wrong input, please type again:")
+            return Activity
+
+
 
 
 
 
 
 def main():
-    User.action()
-    userName = input("\nPlease let us know your Name: ")
-    userPassword = input("\nPlease enter your Password: ")
-    user = User(userName, userPassword)
+    action()
+    username = input("\nPlease let us know your Name: ")
+    userpassword = input("\nPlease enter your Password: ")
+    user = User(username, userpassword)
 
     while True:
         choice = input("Do you want to add Spending? please type Yes/No")
         if choice == "No":
-            user.printSpendings()
+            user.print_spendings()
             print("Thanks for using our App")
             break
         else:
-            activity = Activity.use_activity()
+            activity = Activity
+            activity.choose_activity(activity)
             day = input("Please type the day of the week: ")
             amount = input("Please type the amount: ")
 
-            user.addSpending(activity, day, amount)
+            user.add_spending(activity, day, amount)
 
 
 main()
